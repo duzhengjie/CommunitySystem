4a4d6c47547748750512f99b9a72321cb5acc1ba
duzhengjie <526415719@qq.com>
2018-03-28 19:40:27 +0800

    编码规范，顺便测试一发
    Signed-off-by: bei <buyu11@qq.com>

--- src/main/java/com/communitySystem/controller/UserController.java
+++ src/main/java/com/communitySystem/controller/UserController.java
@@ -18,9 +18,9 @@ public class UserController {
 
     @ResponseBody
     @RequestMapping("/showUser.do")
-    public Map<String,User> selectUser(Map<String,User> m,@RequestParam(required = false) long id){
+    public Map<String, User> selectUser(Map<String,User> m,@RequestParam(required = false) long id){
         User user = userService.selectUser(id);
-        m.put("Users",user);
+        m.put("data",user);
         System.out.println(user);
         return m;
     }
